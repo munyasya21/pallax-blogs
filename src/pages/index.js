1d9947039a5cf62581e46a64316a7e9a64d40166
munyasya21 <munyasyacynthia@gmail.com>
2024-10-17 17:11:39 +0300

    button

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@
 
 export default function Home() {
   return (
-    <div className="bg-white" >
-    <div className="pt-48 pl-48">
+    <div className="" >
+      <div className="pt-48 pl-48">
       <h1 className="font-sans text-6xl font-bold">Blogs</h1>
       <p className="pt-10">Welcome to our website, your go-to destination for finding the perfect apartment!<br/>
        Whether you're looking for a cozy studio, a spacious family flat, or a luxury penthouse,<br/>
@@ -12,6 +12,11 @@ export default function Home() {
        tools, detailed property descriptions, and high-quality images, we make it easy for you to<br/>
        explore, compare, and choose your next home. Start your journey with us today and find an <br/>
        apartment that fits your lifestyle and budget!</p>
+       <div className="pt-2">
+       <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl">
+        Explore Apartments
+       </button>
+       </div> 
     </div>
     </div>
   );
