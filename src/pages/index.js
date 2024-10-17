ae9f749c0d3563e73d14eeba6bfe11904c5280ad
munyasya21 <munyasyacynthia@gmail.com>
2024-10-17 16:17:32 +0300

    blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,14 @@
 export default function Home() {
   return (
     <div className="bg-white" >
-    <div>
-      <h1>Blogs</h1>
+    <div className="pt-48 pl-48">
+      <h1 className="font-sans text-6xl font-bold">Blogs</h1>
+      <p className="pt-10">Welcome to our website, your go-to destination for finding the perfect apartment!<br/>
+       Whether you're looking for a cozy studio, a spacious family flat, or a luxury penthouse,<br/>
+       we offer a diverse range of listings tailored to suit your needs. With user-friendly search<br/>
+       tools, detailed property descriptions, and high-quality images, we make it easy for you to<br/>
+       explore, compare, and choose your next home. Start your journey with us today and find an <br/>
+       apartment that fits your lifestyle and budget!</p>
     </div>
     </div>
   );
